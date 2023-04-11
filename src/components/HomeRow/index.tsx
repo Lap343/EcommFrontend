39162f9914bb52fde4c750613db17ad1901d1c45
Luis Perez <44172765+Lap343@users.noreply.github.com>
2023-04-11 10:30:28 -0700

    feat(home-icons): style home row icons to match the figma designs

--- src/components/HomeRow/index.tsx
+++ src/components/HomeRow/index.tsx
@@ -10,15 +10,23 @@ import {
 const HomeRowStyles = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-around;
   width: 100vw;
   height: 10vh;
+  background-color: #262626;
+  border-radius: 0px 0px 25px 25px;
+
+  & svg {
+    height: auto;
+    width: 2em;
+    color: #414141;
+  }
 `;
 
 const HomeRow = () => {
   return (
     <HomeRowStyles>
-      <BiHomeAlt />
+      <BiHomeAlt style={{ color: "#E1FF6B" }} />
       <BiSearchAlt2 />
       <BiCartAlt />
       <BiInfoCircle />
